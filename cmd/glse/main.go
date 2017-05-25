bfbfaa0fe9fa7d2c1739823558cde144fa57d3f4
sky0621 <sky14work@gmail.com>
2017-05-26 01:41:01 +0900

    bug fix

--- cmd/glse/main.go
+++ cmd/glse/main.go
@@ -11,11 +11,6 @@ import (
 	gitlab "github.com/xanzy/go-gitlab"
 )
 
-var (
-	projectOrderByColumn = "name"
-	projectSort          = "asc"
-)
-
 // TODO 機能実現スピード最優先での実装なので要リファクタ
 func main() {
 	host := flag.String("host", "example.com", "GitLab Host Name")
@@ -41,8 +36,8 @@ func main() {
 	glNss := []*gitlabNameSpace{}
 	for _, ns := range namespaces {
 		projects, res, err := git.Projects.ListProjects(&gitlab.ListProjectsOptions{
-			OrderBy: &projectOrderByColumn,
-			Sort:    &projectSort,
+			OrderBy: gitlab.String("name"),
+			Sort:    gitlab.String("asc"),
 			ListOptions: gitlab.ListOptions{
 				Page:    1,
 				PerPage: 1000,
@@ -56,12 +51,13 @@ func main() {
 		}
 
 		glPrjs := []*gitlabProject{}
-		for idx, p := range projects {
+		no := 1
+		for _, p := range projects {
 			if ns.Path != p.Namespace.Path {
 				continue
 			}
 			glPrjs = append(glPrjs, &gitlabProject{
-				No:             idx + 1,
+				No:             no,
 				ID:             p.ID,
 				NamespaceID:    p.Namespace.ID,
 				Namespace:      p.Namespace.Name,
@@ -71,6 +67,7 @@ func main() {
 				WebURL:         p.WebURL,
 				LastActivityAt: p.LastActivityAt.Format("2006-01-02 15:04:05"),
 			})
+			no = no + 1
 		}
 
 		glNss = append(glNss, &gitlabNameSpace{Path: ns.Path, Projects: glPrjs})
